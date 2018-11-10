1e215dc50bd29f07335ffd9bf45b94330eda841e
Chuck Mortimore <cmortimore@salesforce.com>
2018-11-10 13:31:13 -0800

    emojis

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var emoji = require('emoji.json')
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', { emojis: emoji}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

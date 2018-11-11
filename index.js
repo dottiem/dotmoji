dd40894107ee7d900095445fe5f5185841d4ef1d
Chuck Mortimore <cmortimore@salesforce.com>
2018-11-10 17:01:31 -0800

    translator

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const PORT = process.env.PORT || 5000
 var emoji = require('emoji.json')
 
 
+var secretDictionary = new Object();
+secretDictionary["i"] = emoji[404].char;
+
+
 var app = express().use(express.static(path.join(__dirname, 'public'))).set('views', path.join(__dirname, 'views')).set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index', { emojis: emoji}))
+app.get('/', (req, res) => res.render('pages/index', {}))
 
 app.get('/translator', (req, res) => res.render('pages/translator', {}))
 
+app.get('/emojis', (req, res) => res.render('pages/emojis', { emojis: emoji } ))
 
 app.get('/translate/:id', function(request, response) {
+	
+	
 	
 	var number = request.params.id;
 	var words = request.params.id.split(' ');
@@ -20,7 +27,12 @@ app.get('/translate/:id', function(request, response) {
 	var pos = 0;
 	words.forEach(word => {
 		if (word != null && word != "") {
-			translation[pos] = emoji[word].char;
+			var lowercaseWord = word.toLowerCase();
+			if (lowercaseWord in secretDictionary) {
+				translation[pos] = secretDictionary[lowercaseWord]; 
+			} else {
+				translation[pos] = word;
+			}
 			pos++;
 		}
 	});
